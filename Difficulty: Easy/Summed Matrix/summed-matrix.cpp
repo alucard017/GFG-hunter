549c27a5157ad18a0fce3759002006fa85e1d2be
Apurba Sundar Nayak <76940364+alucard017@users.noreply.github.com>
2024-06-24 23:45:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Summed Matrix/summed-matrix.cpp
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    long long sumMatrix(long long n, long long q) {
+        // code here
+        long long cnt = 0;
+        if (q >= 2 && q <= 2 * n) {
+            cnt = min(q - 1, 2 * n - q + 1);
+        }
+        return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long n,q;
+        
+        cin>>n>>q;
+
+        Solution ob;
+        cout << ob.sumMatrix(n,q) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
